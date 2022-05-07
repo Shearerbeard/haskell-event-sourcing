f255d3976f5dc38b06d036bda310f836cf53103f
Mike Shearer <shearerbeard@gmail.com>
2022-05-07 11:55:23 -0600

    playing with adding payment

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,22 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
+import Data.ByteString (ByteString)
+import Data.Either
+import Data.Maybe
+import Data.String
 import Lib
+import Web.Stripe
+import Web.Stripe.Customer
+import Prelude (IO, fromInteger, print, ($))
 
 main :: IO ()
-main = someFunc
+main = do
+  let key = "sk_test_51KtfMBHho03e1UbZtFkaAoCBBRLb45qg2zeoWFyYn55l8fMFvVrcIIXozhRS3RThiQqu4m9aCI1xf9YDjUpNPizD001ZcXIbp3"
+  let config = StripeConfig (StripeKey key) Nothing
+  result <- stripe config $ getCustomers -&- Limit 100
+
+  case result of
+    Right stripelist -> print (list stripelist :: [Customer])
+    Left stripeError -> print stripeError
